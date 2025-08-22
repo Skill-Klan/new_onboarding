e2d95c2f6ff10fb4154b57b40103885fc5cee880
Roman Martsenko <116177826+RomanBrov@users.noreply.github.com>
2025-08-22 22:56:56 +0300

    first structure

---
+++ miniapp/src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { WebApp } from './core/telegram'
+import './styles/main.css'
+
+const app = createApp(App)
+app.mount('#app')
+
+// ініціалізація Telegram Mini App
+if (WebApp?.ready) {
+  WebApp.ready()
+}
